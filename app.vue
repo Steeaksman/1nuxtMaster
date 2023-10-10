88425f7d1811f3b8d4b219b9c1b0d56f6d19db96
Steeaksman <nstokes142@gmail.com>
2023-10-09 21:54:29 -0700

    added exclamation mark

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">
-    This is a nuxt master with tailwind setup
+    This is a nuxt master with tailwind setup111
   </h1>
   </div>
 </template>
